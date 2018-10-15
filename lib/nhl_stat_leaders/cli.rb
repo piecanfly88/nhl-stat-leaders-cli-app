59a38ba2d0fdfaff5d8de55931ace1e9aa7952fa
Richard "Scott" Salchunas <salchunr@gmail.com>
2018-10-15 19:11:56 -0400

    Add #process_selection to cli.rb, establish logic for functionality

--- lib/nhl_stat_leaders/cli.rb
+++ lib/nhl_stat_leaders/cli.rb
@@ -7,12 +7,39 @@ class NhlStatLeaders::CLI
   end
 
   def main_menu
-    puts "Please select from the following options"
-    puts "1. View leaders by stat"
-    puts "2. View leaders by team"
-    puts "3. Search Player"
+    puts "Please select from the following options by entering their corresponding numerical value"
+    puts "If you wish to return to this menu at any point simply enter 'menu'"
+    puts "If you wish to exit the program simply enter 'exit'"
+    puts ""
+    puts "1. View Stat Leaders"
+    puts "2. View Team Leaders"
+    puts "3. Search Player Stats"
     puts "4. Glossary"
   end
+
+  def process_selection
+    input = nil
+    while input != "exit"
+      input = gets.strip.to_i
+
+      if input == 1
+        #sort_stats
+        #display_stats
+      elsif input == 2
+        #get_team_roster
+        #sort_stats
+        #display_stats
+      elsif input == 3
+        #find_player
+        #display stats
+      elsif input == 4
+        glossary
+      else
+        puts "Enter 'menu' to return to the main menu or 'exit' to leave the program"
+      end
+    end
+  end  
+
 
   def glossary
     puts "list NHL stats and their descriptions"
